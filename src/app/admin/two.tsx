b49f585181c16a3e73e1c915c1b977b2d9844ac0
ahsan-19 <112505556+ahsan-19@users.noreply.github.com>
2024-10-20 19:40:08 +0500

    video followed till here , now i will be doing diff stuff for mds

---
+++ src/app/admin/two.tsx
@@ -0,0 +1,27 @@
+import { StyleSheet, View , Text } from 'react-native';
+
+
+export default function TabTwoScreen() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Tab Two</Text>
+      <Text style={styles.Content}> hi i m tab 2</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+   // color:'white'
+  },
+  Content : {
+     //color:'white'
+  }
+});
